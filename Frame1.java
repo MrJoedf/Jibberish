69e151774f40bb2969f0d21fa7d2902ef38f3814
MrJoedf <joefoster0011@gmail.com>
2019-05-18 01:21:10 -0500

    Update Frame1.java

--- Frame1.java
+++ Frame1.java
@@ -22,6 +22,8 @@ public class Frame1 {
 	 */		
 	
 	public static void main(String[] args) {
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
+		System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Name");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
