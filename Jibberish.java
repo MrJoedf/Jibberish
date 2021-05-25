e8e9c1cff39a9e0ff140e0760f02e176451709f7
MrJoedf <joefoster0011@gmail.com>
2021-05-25 07:33:09 -0500

    Update Jibberish.java

--- Jibberish.java
+++ Jibberish.java
@@ -1,5 +1,8 @@
 
 import java.awt.BorderLayout;
+
+import javax.swing.tree.TreePath;
+
 import java.awt.Dimension;
 import java.awt.EventQueue;
 
@@ -29,12 +32,15 @@ import java.io.IOException;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.MatteBorder;
 import java.awt.Color;
+import java.awt.Component;
+
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.SoftBevelBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 import javax.swing.JTextArea;
@@ -75,6 +81,29 @@ public class Jibberish extends JFrame {
     public String rootPath;
     
     boolean nightMode;
+    File[] dirs;
+    
+    //Sourced from https://coderanch.com/t/641799/java/Custom-Jtree-renderer
+    //Fix the appearance of trees and prevents the full path from being shown
+    private static class JibTreeRenderer extends DefaultTreeCellRenderer {  
+    	 
+        @Override 
+        public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {  
+     
+            if(value instanceof DefaultMutableTreeNode){  
+     
+                DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;  
+                Object userValue = node.getUserObject();  
+     
+                if(userValue instanceof File){  
+                     // overwrite value with the new value to renderer; this is a local parameter/variable, you're not changing the actual content in the tree
+                     value = ((File) userValue).getName();
+                }  
+               
+            }  
+            return super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);  
+        }  
+    }
     
     private void createChildren(File fileRoot, 
             DefaultMutableTreeNode node) {
@@ -82,23 +111,34 @@ public class Jibberish extends JFrame {
         if(files == null){return;}
 
         for (File file : files) {
-            DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(file);
-            node.add(childNode);
+        		TreePath temp = new TreePath(file);
+        		
+            DefaultMutableTreeNode childNode = new DefaultMutableTreeNode(temp.getLastPathComponent());
+            //childNode.
+          //  
+           
+           //childNode.setUserObject(file.getName());
             
+            node.add(childNode);
+        
            
             
            if (file.isDirectory()) {
+        	   		//node.setUserObject(file.getName());
                 createChildren(file, childNode);
             }
         }
+       
     }
-
-
+    
+    
     
 	public Jibberish(String projectName, boolean newProject){
 		nightMode=false;
 		
-		setTitle(projectName + " - Jibberish");
+		File pName = new File(projectName);
+		
+		setTitle(pName.getName() + " - Jibberish");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1368, 765);
 		
@@ -140,7 +180,7 @@ public class Jibberish extends JFrame {
 		
 		if(newProject) {
 			 fileroot = new File("Jibberish"+File.separator+projectName);
-		}else{
+		}else {
 			 fileroot=new File(projectName);
 		}
 		root = new DefaultMutableTreeNode(fileroot);
@@ -154,6 +194,8 @@ public class Jibberish extends JFrame {
 		JTree draftTree = new JTree(treeModel);
 		draftTree.setShowsRootHandles(true);
 		
+		//CALLS THE INSTANCE OF THE CELL RENDER TO PREVENT SHOWING THE FULL PATH
+		draftTree.setCellRenderer(new JibTreeRenderer());
 		
 		JScrollPane treeScroll = new JScrollPane(draftTree);
 		treeScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
@@ -177,8 +219,7 @@ public class Jibberish extends JFrame {
 		contentPane.add(sp);
 		editorPane_1.setBounds(450, 39, 640, 860);
 		editorPane_1.setFont(new Font("Arial", Font.PLAIN, 20));
-
-		});
+	
 		draftTree.addTreeSelectionListener(new TreeSelectionListener() {
 
             public void valueChanged(TreeSelectionEvent e) {
@@ -252,12 +293,18 @@ public class Jibberish extends JFrame {
 		
 		newSnippet.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-
+				
+				//fix this, man
+				
 				//TreePath tp = draftTree.getLastSelectedPathComponent();
 				
 				//if(tp==null) {return;}
+		
+		
+				File temp  = new File(draftTree.getSelectionPath().toString());
 				
-				String snipPath = draftTree.getLastSelectedPathComponent().toString();
+				String snipPath = temp.getPath();
+				snipPath = snipPath.substring(1, snipPath.length() - 1);
 				
 				File snipRoot= new File(snipPath);
 				
@@ -265,13 +312,15 @@ public class Jibberish extends JFrame {
 					snipRoot = snipRoot.getParentFile();
 				}*/
 				
-				System.out.println(snipRoot);
+				System.out.println("SNIP ROOT:"+ snipRoot);
 				draftTree.setName("Draft Tree Name here");
 				newSnippet snip = new newSnippet(snipRoot, draftTree);	
 				
 				snip.frmSnippet.setVisible(true);
 				
 				//createChildren(fileroot, root);
+				draftTree.setCellRenderer(new JibTreeRenderer());
+				draftTree.updateUI();
 				
 			
 			}
@@ -280,12 +329,9 @@ public class Jibberish extends JFrame {
 				// TODO Auto-generated method stub
 				return null;
 			}
-		});
-		
-		
-			
-		
+		});	
 	}
+	
 public Jibberish(){
 		//default constructor for the first calling
 		//use for testing and making sure things don't get wonky
@@ -340,23 +386,27 @@ public Jibberish(){
 		contentPane.add(newSnippet);
 		contentPane.add(newFolder);
 		
-			
+		
+		
 		JTree draftTree = new JTree();
 		draftTree.setFont(new Font("Tahoma", Font.BOLD, 16));
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		tabbedPane_1.addTab("Draft", null, draftTree, null);	
+		tabbedPane_1.addTab("Draft", null, draftTree, null);
 		
 		
 		JEditorPane editorPane_1 = new JEditorPane();
 		JScrollPane sp = new JScrollPane(editorPane_1);
-			
+		
+		
 		
 		sp.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		sp.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 		sp.getVerticalScrollBar();
 		sp.setBounds(350,39,720,600);
 		contentPane.add(sp);
-		editorPane_1.setBounds(350, 39, 720, 600);	
+		editorPane_1.setBounds(350, 39, 720, 600);
+		
+
 		
 	}
 }
